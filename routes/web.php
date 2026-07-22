f0412e2590bf56110d929078bd8ab0f841ac50e8
bachh8172-art <bachh8172@gmail.com>
2026-07-22 10:03:53 +0700

    feat(admin): create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,19 @@ Route::get(
     [FrontendHomestayController::class, 'show']
 )->name('homestays.show');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+    });
 
 require __DIR__.'/auth.php';

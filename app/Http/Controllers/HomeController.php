c2437f36aadc578aa824aa9a5f21b3244d56c1ef
bachh8172-art <bachh8172@gmail.com>
2026-07-22 11:04:36 +0700

    feat(search): search homestay by city and address

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Homestay;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
-        $homestays = Homestay::query()
+        $query = Homestay::query()
             ->with('category')
-            ->where('status', true)
+            ->where('status', true);
+
+        if ($request->filled('keyword')) {
+            $query->where('name', 'like', '%' . $request->keyword . '%');
+        }
+
+        if ($request->filled('location')) {
+            $location = $request->location;
+
+            $query->where(function ($subQuery) use ($location) {
+                $subQuery
+                    ->where('city', 'like', '%' . $location . '%')
+                    ->orWhere('address', 'like', '%' . $location . '%');
+            });
+        }
+
+        $homestays = $query
             ->latest()
-            ->paginate(6);
+            ->paginate(6)
+            ->withQueryString();
 
         return view('home.index', compact('homestays'));
     }

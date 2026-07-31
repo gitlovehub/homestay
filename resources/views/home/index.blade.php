7b8ebd04ef2c29e25a32ac9df3b0b31d43e3443c
bachh8172-art <bachh8172@gmail.com>
2026-07-31 21:31:28 +0700

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -327,7 +327,7 @@
                 </div>
 
                 <div class="flex flex-wrap items-center gap-3">
-                    <
+                    <a
                         href="{{ route('home') }}"
                         class="inline-flex items-center justify-center rounded-xl border border-slate-200 bg-white px-5 py-3 text-sm font-semibold text-slate-700 shadow-sm transition hover:border-blue-200 hover:bg-blue-50 hover:text-blue-700"
                     >

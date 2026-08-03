cf1c3293ec2bb202c237e2ff12c72e160d3a2327
gitlovehub <hungtd010302@gmail.com>
2026-08-03 08:01:55 +0700

    Done

--- lang/vi/passwords.php
+++ lang/vi/passwords.php
@@ -2,7 +2,7 @@
 
 return [
     'reset' => 'Mật khẩu của bạn đã được đặt lại thành công.',
-    'sent' => 'Liên kết đặt lại mật khẩu đã được gửi. Vui lòng kiểm tra email hoặc file log.',
+    'sent' => 'Liên kết đặt lại mật khẩu đã được gửi. Vui lòng kiểm tra Email của bạn.',
     'throttled' => 'Bạn vừa yêu cầu đặt lại mật khẩu. Vui lòng đợi một lúc rồi thử lại.',
     'token' => 'Liên kết đặt lại mật khẩu không hợp lệ hoặc đã hết hạn.',
     'user' => 'Không tìm thấy tài khoản sử dụng địa chỉ email này.',
